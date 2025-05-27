f55587a9d3b0c7b1f3018d7c6d95345ec35f980a
MgPhenix <143949086+MgPhenix@users.noreply.github.com>
2025-05-27 10:52:05 +0200

    Update main.py
    Some test for the futur main file

--- InDevVersion/main.py
+++ InDevVersion/main.py
@@ -1,5 +1,10 @@
 from MgSqlite import *
+import os
 
 
+def deleteDatabase(name):
+     try:
+         os.remove(name+".db")
+     except:
+         name.delete(). #not implement yet 
 
-data = Database("data")
