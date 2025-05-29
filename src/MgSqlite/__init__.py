9e03c2073653c1be6982e992418f2ec1aa087085
MgPhenix <143949086+MgPhenix@users.noreply.github.com>
2025-05-29 21:25:46 +0200

    Correction for __init__.py

--- src/MgSqlite/__init__.py
+++ src/MgSqlite/__init__.py
@@ -1,9 +1,9 @@
 
 """
-MgSqlite bruh
+MgSqlite is module for create and interract with database without using sql, the module use sqlite3 but for create and interract with database you don't need to code in sql just use all the functions
 """
 
-__version__ = "0.04"
+__version__ = "0.5"
 
 from .database import Database
 from .sql_interraction import SQL_Execution
