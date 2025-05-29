5c781cca241a3b319d4b8af61deadb7d8d1e79de
MgPhenix <143949086+MgPhenix@users.noreply.github.com>
2025-05-29 21:18:29 +0200

    Archived MgSqlite 0.4

---
+++ archived_version/MgSqlite_0.4/database.py
@@ -0,0 +1,29 @@
+import os
+from sql_interraction import SQL_Execution
+
+class Database(SQL_Execution):
+    def __init__(self, name : str = "default") -> None:
+        super().__init__()                        
+        self.databaseName = str(name)+".db"
+        self.tableList = []
+        self.printSQL = False                               
+        self._tryConnect(self.databaseName)
+        
+    def turnOffPrintSQL(self) -> None:
+        self.printSQL = False
+
+    def turnOnPrintSQL(self) -> None:
+        self.printSQL = True
+
+    def deleteAllTable(self) -> None:
+        try:
+            for table in self.tableList:
+                table.deleteTable()
+        except:
+            print("Can't delete table or table does not exist")    
+            
+    def deleteDatabase(self) -> None:
+        try:
+            os.remove(self.databaseName)
+        except:
+            print("Can't delete database")
